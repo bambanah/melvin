527482acca8919322bce874eb9da5baef63f052a
Lachlan Underhill <lachlanu@gmail.com>
2021-03-31 06:51:42 +1000

    Changed date string format from dashes to slashes

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -8,7 +8,7 @@ export const formatDate = (timestamp: firebase.firestore.Timestamp) => {
 	const MM = ("0" + (date.getMonth() + 1)).slice(-2);
 	const DD = ("0" + date.getDate()).slice(-2);
 
-	return `${DD}-${MM}-${YYYY}`;
+	return `${DD}/${MM}/${YYYY}`;
 };
 
 export const stringToTimestamp = (timeValue: string) => {
