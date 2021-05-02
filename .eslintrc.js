e5a1ed5f1dc2502b5070b4c2683c02d9ff5d916c
Lachlan Underhill <lachlan.underhill@workingmouse.com.au>
2021-05-02 15:11:44 +1000

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "./tsconfig.eslint.json",
+	},
+	rules: {
+		"no-console": ["warn", { allow: ["warn", "error"] }],
+		"no-param-reassign": ["error", { props: false }],
+		"react/require-default-props": 0,
+		"react/jsx-props-no-spreading": 0,
+	},
+	plugins: ["@typescript-eslint"],
+	extends: ["airbnb-typescript", "prettier"],
+};
