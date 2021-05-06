493a67a57b248669cd1c3b02d77e33ab44f2f348
Lachlan Underhill <lachlan.underhill@workingmouse.com.au>
2021-05-06 19:55:55 +1000

    Added react-toastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import "bulma/css/bulma.css";
+import 'bulma/css/bulma.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-	document.getElementById("root")
+	document.getElementById('root'),
 );
