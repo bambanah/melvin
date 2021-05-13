8806ba0f27c4768b11e94fead89cacc64fc267c4
Lachlan Underhill <lachlan.underhill@workingmouse.com.au>
2021-05-13 20:53:49 +1000

    Created template page

---
+++ pages/templates.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+import Layout from "../components/Layout";
+
+const Templates = () => {
+	return (
+		<Layout>
+			<Head>
+				<title>Templates</title>
+			</Head>
+			<div>Templates go here</div>
+		</Layout>
+	);
+};
+
+export default Templates;
