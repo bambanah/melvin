3d6de244b420a62d3930979162f311268883de90
Lachlan Underhill <lachlanu@gmail.com>
2021-09-07 21:37:29 +1000

    Remove stupid max length on invoice number

--- schema/InvoiceValidationSchema.ts
+++ schema/InvoiceValidationSchema.ts
@@ -1,11 +1,7 @@
 import * as yup from "yup";
 
 const InvoiceValidationSchema = yup.object().shape({
-	client_no: yup
-		.string()
-		.min(5, "Too short")
-		.max(20, "Too long")
-		.required("Required"),
+	client_no: yup.string().required("Required"),
 	client_name: yup.string().required("Required"),
 	bill_to: yup.string().required("Required"),
 	invoice_no: yup.string().required("Required"),
