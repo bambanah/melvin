f4c89f35fddb5cdfb2799044b3f156c892b37011
lachie-underhill <lachlanu@gmail.com>
2023-02-11 16:23:29 +1000

    feat: refactor badge component

--- src/components/atoms/badge.tsx
+++ src/components/atoms/badge.tsx
@@ -1,38 +1,31 @@
-import styled from "styled-components";
+import classNames from "classnames";
+
+type Variant = "DEFAULT" | "INFO" | "SUCCESS" | "WARNING" | "ERROR";
+
+const variantStyles: Record<Variant, string> = {
+	DEFAULT: "bg-slate-200 text-slate-600",
+	INFO: "bg-blue-100 text-blue-500",
+	SUCCESS: "bg-green-200 text-green-600",
+	WARNING: "bg-yellow-100 text-yellow-600",
+	ERROR: "bg-red-100 text-red-600",
+};
 
 interface Props {
 	children: string;
-	variant?: "info" | "success" | "warning" | "danger";
+	variant?: Variant;
 }
 
-const StyledBadge = styled.div`
-	padding: 0.3rem 0.4rem;
-	font-size: 12px;
-	border-radius: 5px;
-	font-weight: bold;
-
-	color: ${({ theme }) => theme.colors.fg};
-	background-color: #dcdfe2;
-
-	&.info {
-		background-color: ${({ theme }) => theme.colors.info};
-	}
-
-	&.success {
-		background-color: ${({ theme }) => theme.colors.success};
-	}
-
-	&.warning {
-		background-color: ${({ theme }) => theme.colors.warning};
-	}
-
-	&.danger {
-		background-color: ${({ theme }) => theme.colors.error};
-	}
-`;
-
-const Badge = ({ children, variant }: Props) => {
-	return <StyledBadge className={variant}>{children}</StyledBadge>;
+const Badge = ({ children, variant = "DEFAULT" }: Props) => {
+	return (
+		<div
+			className={classNames(
+				"bg-b rounded-[5px] py-0.5 px-1 text-[11px] font-bold",
+				variantStyles[variant]
+			)}
+		>
+			{children}
+		</div>
+	);
 };
 
 export default Badge;
