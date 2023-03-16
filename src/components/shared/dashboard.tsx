083d26e119a85d3fbb64e9654317588aa56dc228
lachie-underhill <lachlanu@gmail.com>
2023-03-17 09:28:12 +1000

    feat: add layout to homepage

---
+++ src/components/shared/dashboard.tsx
@@ -0,0 +1,12 @@
+import Display from "@atoms/display";
+
+const Dashboard = () => {
+	return (
+		<div>
+			<Display className="small">Welcome to Melvin!</Display>
+			<p>This is the dashboard</p>
+		</div>
+	);
+};
+
+export default Dashboard;
