65f95e133b6b6a3991bbf5b0b6134b5c3079df0b
lachie-underhill <lachlanu@gmail.com>
2023-03-28 17:29:24 +1000

    feat: very simple activity page

--- src/components/activities/activity-page.tsx
+++ src/components/activities/activity-page.tsx
@@ -1,16 +1,45 @@
+import ConfirmDialog from "@atoms/confirm-dialog";
+import Dropdown from "@atoms/dropdown";
+import Heading from "@atoms/heading";
+import {
+	faEllipsisV,
+	faUser,
+	faClock,
+	faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { trpc } from "@utils/trpc";
 import dayjs from "dayjs";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const SupportItemPage = () => {
 	const router = useRouter();
+	const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+
 	const activityId = String(router.query.id);
 
+	const trpcContext = trpc.useContext();
 	const { data: activity, error } = trpc.activity.byId.useQuery({
 		id: activityId,
 	});
+	const deleteActivityMutation = trpc.activity.delete.useMutation();
+
+	const deleteActivity = () => {
+		deleteActivityMutation
+			.mutateAsync({ id: activityId })
+			.then(() => {
+				trpcContext.activity.list.invalidate();
+				toast.success("Activity deleted");
+				router.push("/activities");
+			})
+			.catch(() => {
+				toast.error("An error occured. Please refresh and try again.");
+			});
+	};
 
 	if (error) {
 		console.error(error);
@@ -22,21 +51,72 @@ const SupportItemPage = () => {
 		<div className="flex flex-col items-center justify-center px-8">
 			<Head>
 				<title>
-					{`${dayjs(activity.date).format("dd/mm")} - ${
-						activity.supportItem.description
-					} - Melvin`}
+					{`${activity.supportItem.description} - ${dayjs(activity.date).format(
+						"DD/MM h:mm"
+					)} | Melvin`}
 				</title>
 			</Head>
-			<div className="flex flex-col gap-4 px-3">
-				<Link href="/activities">&lt; Back to Activities</Link>
-				<h1>{activity.supportItem.description}</h1>
-				<p>{dayjs(activity.date).format("DD/MM/YY")}</p>
+			<div className="flex w-full max-w-4xl flex-col gap-4 px-3">
+				<ConfirmDialog
+					title="Are you sure you want to delete this activity?"
+					description="This cannot be undone."
+					isOpen={isDeleteDialogOpen}
+					setIsOpen={setIsDeleteDialogOpen}
+					confirmText="Delete"
+					cancelText="Cancel"
+					confirmAction={deleteActivity}
+				/>
 
-				<Link href={`/activities/${activityId}/edit`}>Edit</Link>
+				<div className="mb-2 flex items-center justify-between">
+					<Heading className="medium text-lg sm:text-2xl">
+						{activity.supportItem.description}
+					</Heading>
 
-				<a href={`/clients/${activity.client?.id}`}>{activity.client?.name}</a>
-				<p>Start Time: {dayjs(activity.startTime).format("hh:mma")}</p>
-				<p>End Time: {dayjs(activity.endTime).format("hh:mma")}</p>
+					<Dropdown>
+						<Dropdown.Button>
+							<FontAwesomeIcon icon={faEllipsisV} />
+						</Dropdown.Button>
+						<Dropdown.Items>
+							<Dropdown.Item>
+								<Link
+									href={`/activities/${activity.id}/edit`}
+									className="px-3 py-4 text-neutral-900 hover:bg-neutral-100 sm:py-2"
+								>
+									Edit
+								</Link>
+							</Dropdown.Item>
+							<Dropdown.Item>
+								<button
+									type="button"
+									className="px-3 py-4 text-left text-neutral-900 hover:bg-neutral-100 sm:py-2"
+									onClick={() => setIsDeleteDialogOpen(true)}
+								>
+									Delete
+								</button>
+							</Dropdown.Item>
+						</Dropdown.Items>
+					</Dropdown>
+				</div>
+
+				<div className="flex items-center gap-2">
+					<FontAwesomeIcon icon={faCalendar} />
+					<p>{dayjs(activity.date).format("DD/MM/YY")}</p>
+				</div>
+
+				<div className="flex items-center gap-2">
+					<FontAwesomeIcon icon={faClock} />
+					<p>
+						{dayjs(activity.startTime).format("hh:mma")} -{" "}
+						{dayjs(activity.endTime).format("hh:mma")}
+					</p>
+				</div>
+
+				<p className="flex items-center gap-2">
+					<FontAwesomeIcon icon={faUser} />
+					<Link href={`/clients/${activity.client?.id}`}>
+						{activity.client?.name}
+					</Link>
+				</p>
 			</div>
 		</div>
 	);
