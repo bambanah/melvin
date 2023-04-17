92a1c85f53f5db9e05bbb67760dd90b42c6d9a46
lachie-underhill <lachlanu@gmail.com>
2023-04-17 16:40:02 +1000

    feat: implement invoice copying

--- src/pages/invoices/create.tsx
+++ src/pages/invoices/create.tsx
@@ -2,6 +2,10 @@ import Layout from "@components/shared/layout";
 import CreateInvoiceForm, {
 	FormValues,
 } from "@components/invoices/invoice-form";
+import prisma from "@server/prisma";
+import { GetServerSideProps } from "next";
+import { invoiceToValues } from "@utils/formik-utils";
+import { getNextInvoiceNo } from "@utils/invoice-utils";
 
 interface Props {
 	initialValues?: FormValues;
@@ -17,6 +21,63 @@ const CreateInvoice = ({ initialValues, copiedFrom }: Props) => {
 			/>
 		</Layout>
 	);
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+	const invoiceId = query.copyFrom;
+
+	if (invoiceId) {
+		const invoice = await prisma.invoice.findUnique({
+			where: {
+				id: String(invoiceId),
+			},
+			include: {
+				activities: {
+					include: {
+						supportItem: true,
+					},
+				},
+				client: true,
+			},
+		});
+
+		if (invoice) {
+			const invoiceNumbers = await prisma.invoice.findMany({
+				where: {
+					ownerId: invoice.ownerId,
+				},
+				select: {
+					invoiceNo: true,
+				},
+			});
+
+			const initialValues = invoiceToValues(invoice);
+
+			initialValues.invoiceNo = getNextInvoiceNo(
+				invoiceNumbers.map((i) => i.invoiceNo)
+			);
+
+			initialValues.activities = initialValues.activities.map((activity) => {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				const { id, date, ...rest } = activity;
+
+				return rest;
+			});
+
+			delete initialValues.id;
+			delete initialValues.date;
+
+			return {
+				props: {
+					initialValues,
+				},
+			};
+		}
+	}
+
+	return {
+		props: {},
+	};
 };
 
 export default CreateInvoice;
