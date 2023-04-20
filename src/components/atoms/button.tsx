6c6290fb2468a58df77bca147d17563ed53ce79d
lachie-underhill <lachlanu@gmail.com>
2023-04-20 12:41:19 +1000

    feat: add styling for disabled buttons

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -4,11 +4,15 @@ import { ButtonHTMLAttributes } from "react";
 type Variant = "primary" | "secondary" | "success" | "danger" | "default";
 
 const variantStyles: Record<Variant, string> = {
-	primary: "bg-blue_pastel text-fg border-none font-semibold",
-	secondary: "bg-fg text-bg font-semibold",
-	success: "bg-green-pastel font-semibold",
-	danger: "bg-red-500 font-semibold",
-	default: "bg-bg text-fg",
+	primary:
+		"bg-blue_pastel text-fg border-none font-semibold disabled:bg-blue-200 disabled:text-gray-400",
+	secondary:
+		"bg-fg text-bg font-semibold disabled:bg-gray-200 disabled:text-gray-400",
+	success:
+		"bg-green-pastel font-semibold disabled:bg-green-50 disabled:text-gray-400",
+	danger:
+		"bg-red-500 font-semibold disabled:bg-gray-200 disabled:text-gray-400",
+	default: "bg-bg text-fg disabled:bg-gray-200 disabled:text-gray-400",
 };
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -25,7 +29,9 @@ const Button = ({
 	return (
 		<button
 			className={classNames([
-				"flex cursor-pointer items-center justify-center gap-2 border border-fg py-2 px-4 transition-all duration-100 hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[0.25rem_0.25rem_#000]",
+				"flex cursor-pointer items-center justify-center gap-2 border border-fg px-4 py-2 transition-all duration-100",
+				"hover:-translate-x-1 hover:-translate-y-1 hover:shadow-[0.25rem_0.25rem_#000]",
+				"disabled:cursor-default disabled:border-gray-400 disabled:hover:translate-x-0 disabled:hover:translate-y-0 disabled:hover:shadow-none",
 				variantStyles[variant],
 				className,
 			])}
