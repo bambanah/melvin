4fe2cb835dfc51ef4be3595b2fb4bd0fa65c3fa8
lachie-underhill <lachlanu@gmail.com>
2023-05-13 21:22:12 +1000

    fix: required fields not being set as such in activity schema

--- src/schema/activity-schema.ts
+++ src/schema/activity-schema.ts
@@ -3,12 +3,15 @@ import { z } from "zod";
 export const activitySchema = z.object({
 	id: z.string().optional(),
 	supportItemId: z.string(),
-	clientId: z.string(),
+	clientId: z.string().min(1, "Client is required"),
 	date: z
 		.string()
-		.regex(/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/, "WRONG"),
-	startTime: z.string(),
-	endTime: z.string(),
+		.regex(
+			/^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$/,
+			"Date is required"
+		),
+	startTime: z.string().min(1, "Start time is required"),
+	endTime: z.string().min(1, "End time is required"),
 	itemDistance: z.number().nullish(),
 	transitDistance: z.number().nullish(),
 	transitDuration: z.number().nullish(),
