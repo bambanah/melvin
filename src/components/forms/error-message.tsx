60551b2417e6c1ccf8f410a24232cc76665d3f97
lachie-underhill <lachlanu@gmail.com>
2023-05-17 08:59:29 +1000

    feat: error message animates in and out

--- src/components/forms/error-message.tsx
+++ src/components/forms/error-message.tsx
@@ -1,11 +1,22 @@
+import classNames from "classnames";
+
 interface ErrorProps {
 	error?: string;
+	className?: string;
 }
 
-const ErrorMessage = ({ error }: ErrorProps) => {
-	if (!error) return <></>;
-
-	return <p className="mt-1 mb-0 h-4 text-sm text-red-500">{error}</p>;
+const ErrorMessage = ({ error, className }: ErrorProps) => {
+	return (
+		<p
+			className={classNames([
+				"mb-0 text-sm text-red-500 transition-all duration-100 ease-in-out",
+				error ? "mt-1 max-h-4" : "max-h-0",
+				className,
+			])}
+		>
+			{error ?? <br />}
+		</p>
+	);
 };
 
 export default ErrorMessage;
--- src/schema/support-item-schema.ts
+++ src/schema/support-item-schema.ts
@@ -14,7 +14,7 @@ export const supportItemSchema = z.object({
 
 	weekdayCode: zodItemCode.min(1, "Required"),
 	weekdayRate: z
-		.number()
+		.number({ invalid_type_error: "Must be number" })
 		.min(1, "Required")
 		.step(0.01, "Can't be more than 2 decimal places (x.xx)"),
 
