68599590e6157cdf91f3feaf35e8488496df643a
Lachie Underhill <lachlanu@gmail.com>
2023-07-02 20:35:50 +1000

    ci: remove redundant ci envars

--- src/env/schema.js
+++ src/env/schema.js
@@ -4,18 +4,15 @@ const { z } = require("zod");
 const serverSchema = z.object({
 	DATABASE_URL: z.string().url(),
 	NODE_ENV: z.enum(["development", "test", "production"]),
-	NEXTAUTH_SECRET:
-		process.env.NODE_ENV === "production"
-			? z.string().min(1)
-			: z.string().min(1).optional(),
+	NEXTAUTH_SECRET: z.string().min(1).optional(),
 	NEXTAUTH_URL: z.preprocess(
 		(str) => process.env.VERCEL_URL ?? str,
 		process.env.VERCEL ? z.string() : z.string().url()
 	),
-	GOOGLE_ID: z.string(),
-	GOOGLE_SECRET: z.string(),
-	EMAIL_SERVER: z.string(),
-	EMAIL_FROM: z.string(),
+	GOOGLE_ID: z.string().optional(),
+	GOOGLE_SECRET: z.string().optional(),
+	EMAIL_SERVER: z.string().optional(),
+	EMAIL_FROM: z.string().optional(),
 	SEED_EMAIL: z.string().email().optional(),
 });
 
