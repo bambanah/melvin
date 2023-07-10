9fb01ad264f496f8e882f0e0d60d36c8e1bc440a
Lachie Underhill <lachlanu@gmail.com>
2023-07-10 17:52:39 +1000

    fix: supportItem and client select display "Loading..." indefinitely

--- src/components/forms/client-select.tsx
+++ src/components/forms/client-select.tsx
@@ -1,18 +1,24 @@
 import Select from "@components/forms/select";
 import { trpc } from "@utils/trpc";
+import { useEffect, useState } from "react";
 import { FieldValues, UseControllerProps } from "react-hook-form";
 
 function ClientSelect<T extends FieldValues>(props: UseControllerProps<T>) {
+	const [options, setOptions] = useState<{ label: string; value: string }[]>(
+		[]
+	);
 	const { data: { clients } = {} } = trpc.clients.list.useQuery({});
 
-	if (!clients) {
-		return <Select options={[{ label: "Loading...", value: "" }]} {...props} />;
-	}
-
-	const options = clients.map((client) => ({
-		label: client.name,
-		value: client.id,
-	}));
+	useEffect(() => {
+		if (clients) {
+			setOptions(
+				clients.map((client) => ({
+					label: client.name,
+					value: client.id,
+				}))
+			);
+		}
+	}, [clients]);
 
 	return <Select options={options} {...props} />;
 }
