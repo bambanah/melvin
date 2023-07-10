8953db4358cfb1a1ca26ec23d2f6725adbe0cd1f
Lachie Underhill <lachlanu@gmail.com>
2023-07-10 17:32:09 +1000

    chore: add custom inputs to a11y eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,8 @@ module.exports = {
 				labelAttributes: ["label"],
 				controlComponents: [
 					"Input",
+					"Select",
+					"Checkbox",
 					"SupportItemSelect",
 					"ClientSelect",
 					"TimeInput",
