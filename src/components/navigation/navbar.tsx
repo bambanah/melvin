e9511d551de6089c42c86e6724c5da66127c0e06
Lachie Underhill <lachlanu@gmail.com>
2024-03-22 20:07:40 +1000

    feat: fixing some dark mode colors

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -13,32 +13,34 @@ const Navbar = () => {
 	const session = useSession();
 
 	return (
-		<div className="shadow-top fixed bottom-0 flex h-14 w-full items-center gap-8 border-t bg-inherit md:relative md:mx-auto md:h-20 md:max-w-7xl md:border-none md:px-10 md:shadow-none">
-			<div className="flex w-full items-center justify-evenly md:mt-2 md:flex-row md:content-end md:justify-start md:gap-3 md:p-0">
-				<NavLink href="/dashboard/invoices" icon={faFile} className="order-1">
-					Invoices
-				</NavLink>
-				<NavLink href="/dashboard/clients" icon={faUsers} className="order-2">
-					Clients
-				</NavLink>
-				<NavLink
-					href="/dashboard/activities"
-					icon={faRunning}
-					className="order-4"
-				>
-					Activities
-				</NavLink>
-				<NavLink
-					href="/dashboard/support-items"
-					icon={faShoePrints}
-					className="order-5"
-				>
-					Items
-				</NavLink>
-			</div>
-			<div className="flex items-center gap-2">
-				<ModeToggle />
-				<NavAuth user={session.data?.user} />
+		<div className="shadow-top fixed bottom-0 flex h-14 w-full items-center gap-8 border-b border-t bg-inherit md:relative md:h-16 md:px-10">
+			<div className="mx-auto flex w-full max-w-5xl items-center justify-between">
+				<div className="flex w-full items-center justify-evenly md:mx-auto md:flex-row md:content-end md:justify-start md:gap-3 md:p-0">
+					<NavLink href="/dashboard/invoices" icon={faFile} className="order-1">
+						Invoices
+					</NavLink>
+					<NavLink href="/dashboard/clients" icon={faUsers} className="order-2">
+						Clients
+					</NavLink>
+					<NavLink
+						href="/dashboard/activities"
+						icon={faRunning}
+						className="order-4"
+					>
+						Activities
+					</NavLink>
+					<NavLink
+						href="/dashboard/support-items"
+						icon={faShoePrints}
+						className="order-5"
+					>
+						Items
+					</NavLink>
+				</div>
+				<div className="flex items-center gap-2">
+					<ModeToggle />
+					<NavAuth user={session.data?.user} />
+				</div>
 			</div>
 		</div>
 	);
