be4b126966ea826e3e3f3ce8374d2cbfb5f58cd9
Lachie Underhill <lachlanu@gmail.com>
2024-03-25 17:24:03 +1000

    feat: refactor account form

--- src/schema/user-schema.ts
+++ src/schema/user-schema.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-	name: z.string().nullish(),
-	abn: z
+	name: z.string().optional(),
+	abn: z.coerce
 		.number()
 		.min(10_000_000_000, "Must be 11 digits")
 		.max(99_999_999_999, "Must be 11 digits")
-		.nullish(),
-	bankName: z.string().nullish(),
-	bankNumber: z.number().nullish(),
-	bsb: z
+		.optional(),
+	bankName: z.string().optional(),
+	bankNumber: z.coerce.number().optional(),
+	bsb: z.coerce
 		.number()
 		.min(100_000, "Must be 6 digits")
 		.max(999_999, "Must be 6 digits")
-		.nullish(),
+		.optional(),
 });
 export type UserSchema = z.infer<typeof userSchema>;
