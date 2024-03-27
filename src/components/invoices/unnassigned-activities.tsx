2107e5949dd40e202c4c6ef937838e62fff13faa
Lachie Underhill <lachlanu@gmail.com>
2024-03-27 17:31:52 +1000

    style: replace some hard coded colors with variables and replace some FA icons

--- src/components/invoices/unnassigned-activities.tsx
+++ src/components/invoices/unnassigned-activities.tsx
@@ -2,10 +2,10 @@ import Heading from "@/components/ui/heading";
 import { cn } from "@/lib/utils";
 import { InvoiceSchema } from "@/schema/invoice-schema";
 import { ActivityListOutput } from "@/server/api/routers/activity-router";
-import { faCalendar, faCar, faClock } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import dayjs from "dayjs";
+import { Calendar, Car, Clock } from "lucide-react";
 import { UseFormGetValues, UseFormSetValue } from "react-hook-form";
+import { Checkbox } from "../ui/checkbox";
 
 interface Props {
 	activities: ActivityListOutput["activities"];
@@ -17,28 +17,30 @@ const UnassignedActivities = ({ activities, getValues, setValue }: Props) => {
 	return (
 		<div className="flex flex-col gap-2">
 			<Heading size="xsmall">Unassigned Activities</Heading>
+			<p className="text-foreground/80">
+				Select any activities you&#39;d like to add to this invoice.
+			</p>
 
-			<div className="flex flex-col gap-2">
+			<div className="mt-4 flex flex-col gap-2">
 				{activities.length > 0 ? (
 					activities.map((activity) => (
 						<label
 							key={activity.id}
 							className={cn([
-								"flex cursor-pointer gap-2 rounded-md border px-2 py-3 shadow-md transition-shadow hover:border-orange-500 md:p-4",
+								"flex cursor-pointer items-center gap-2 rounded-md border px-2 py-3 shadow-md transition-shadow hover:border-orange-500 md:p-4",
 								getValues("activityIds")?.some((c) => c === activity.id)
-									? "border-orange-300 bg-orange-50"
-									: "bg-white",
+									? "border-primary/20 bg-primary/10"
+									: "bg-background",
 							])}
 							htmlFor={activity.id}
 						>
-							<input
+							<Checkbox
 								id={activity.id}
-								type="checkbox"
 								checked={getValues("activityIds")?.some(
 									(c) => c === activity.id
 								)}
-								onChange={(val) => {
-									if (val.target.checked) {
+								onCheckedChange={(checked) => {
+									if (checked) {
 										const activityIds = getValues("activityIds") ?? [];
 										setValue("activityIds", [...activityIds, activity.id], {
 											shouldTouch: true,
@@ -54,28 +56,26 @@ const UnassignedActivities = ({ activities, getValues, setValue }: Props) => {
 										);
 									}
 								}}
-								className="w-5 cursor-pointer border bg-white outline-none ring-0"
 							/>
 							<div className="flex w-full flex-col gap-2 pl-1 md:pl-4">
 								<p className="font-semibold">
 									{activity.supportItem.description}
 								</p>
 								<div className="flex justify-start gap-6 text-sm md:text-sm">
-									<span>
-										<FontAwesomeIcon icon={faCalendar} />{" "}
+									<div className="flex items-center gap-2">
+										<Calendar className="h-4 w-4" />
 										{dayjs.utc(activity.date).format("dddd DD/MM")}
-									</span>
-									{activity.startTime ? (
-										<span className="">
-											<FontAwesomeIcon icon={faClock} />{" "}
+									</div>
+									{activity.startTime && activity.endTime ? (
+										<div className="flex items-center gap-2">
+											<Clock className="h-4 w-4" />
 											{dayjs.utc(activity.startTime).format("h:mma")}-
 											{dayjs.utc(activity.endTime).format("h:mma")}
-										</span>
+										</div>
 									) : (
-										<span className="">
-											<FontAwesomeIcon icon={faCar} /> {activity.itemDistance}{" "}
-											km
-										</span>
+										<div className="flex items-center gap-2">
+											<Car className="h-4 w-4" /> {activity.itemDistance} km
+										</div>
 									)}
 								</div>
 							</div>
