eaae7944a697ca5681236caf9ba48a81b479881d
Lachie <lachlanu@gmail.com>
2024-10-26 21:27:16 +1000

    feat: add dumb custom rates component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
+import { Prisma } from "@prisma/client";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function decimalToCurrencyString(value: Prisma.Decimal) {
+	return Number(value).toLocaleString(undefined, {
+		style: "currency",
+		currency: "AUD",
+	});
 }
