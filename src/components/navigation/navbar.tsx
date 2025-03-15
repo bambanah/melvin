cf9589551e1878e570ae5af015c88610dcf3914d
Lachie <lachlanu@gmail.com>
2025-03-15 22:20:07 +1000

    chore: upgrade to react 19

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -6,7 +6,7 @@ import MobileNav from "./mobile-nav";
 export function Navbar() {
 	return (
 		<header className="border-border/40 bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 w-full border-b shadow-xs backdrop-blur-sm">
-			<div className="container flex h-14 max-w-(--breakpoint-lg) items-center">
+			<div className="mx-auto flex h-14 max-w-5xl items-center">
 				<MainNav />
 				<MobileNav />
 
