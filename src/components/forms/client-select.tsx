a16235efcd0be38d5f1f5207fce62714e88231f6
Lachie <lachlanu@gmail.com>
2026-02-14 11:57:58 +1000

    chore: upgrade eslint

--- src/components/forms/client-select.tsx
+++ src/components/forms/client-select.tsx
@@ -1,30 +1,27 @@
 import { FormSelect, FormSelectProps } from "@/components/ui/select";
 import { trpc } from "@/lib/trpc";
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
-interface ClientSelectProps
-	extends Omit<FormSelectProps, "placeholder" | "options"> {
+interface ClientSelectProps extends Omit<
+	FormSelectProps,
+	"placeholder" | "options"
+> {
 	excludeClientId?: string;
 }
 
 function ClientSelect({ excludeClientId, ...props }: ClientSelectProps) {
-	const [options, setOptions] = useState<{ label: string; value: string }[]>(
-		[]
-	);
 	const { data: { clients } = {} } = trpc.clients.list.useQuery({});
 
-	useEffect(() => {
-		if (clients) {
-			setOptions(
-				clients
-					.filter((client) => client.id !== excludeClientId)
-					.map((client) => ({
-						label: client.name,
-						value: client.id
-					}))
-			);
-		}
-	}, [clients, excludeClientId]);
+	const options = useMemo(
+		() =>
+			clients
+				?.filter((client) => client.id !== excludeClientId)
+				.map((client) => ({
+					label: client.name,
+					value: client.id
+				})) ?? [],
+		[clients, excludeClientId]
+	);
 
 	return (
 		<FormSelect options={options} placeholder="Select a client..." {...props} />
