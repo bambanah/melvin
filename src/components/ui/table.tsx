61e9e9915ddf51f5bf286d4c5c405589eb915160
Lachie Hughes <lhghs@proton.me>
2026-08-15 21:42:29 +1000

    fix: align table headers with cell horizontal padding

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
 	<th
 		ref={ref}
 		className={cn(
-			"text-muted-foreground h-12 text-left align-middle font-medium [&:has([role=checkbox])]:pr-0",
+			"text-muted-foreground h-12 px-4 text-left align-middle font-medium has-[[role=checkbox]]:pr-0",
 			className
 		)}
 		{...props}
@@ -87,7 +87,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<td
 		ref={ref}
-		className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+		className={cn("p-4 align-middle has-[[role=checkbox]]:pr-0", className)}
 		{...props}
 	/>
 ));
